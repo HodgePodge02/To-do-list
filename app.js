7ff13b744a7289454557c9da1d523ab730847819
HodgePodge02 <false4956@gmail.com>
2022-05-28 21:05:57 -0400

    functions

--- app.js
+++ app.js
@@ -2,20 +2,62 @@
 // 1. DOM selector (getElementByID)
 // 2. Add an event listener on click
 // 3. event listener is taking html and displaying it in a OL format on UI.
-// 4.
 
-// Dom selectors
+let toDo = [];
 
-const MainButton = document.getElementById("MainButton");
+// ---Dom selectors ---
+
+const MainButton = document.getElementById("AddItem");
 const inputDOM = document.getElementById("Input");
+const toDoList = document.getElementById("ToDoList");
 
-// Event listenters
-
-// MainButton.addEventListener(click, (e)) => {
-
-// }
+// ---Event listenters ---
 
 MainButton.addEventListener("click", (e) => {
   const currentInputValue = inputDOM;
   console.log(currentInputValue.value);
+
+  addItem(currentInputValue.value);
+
+  clearList();
+
+  generateList();
+  clearInputField();
 });
+
+// ---functions---
+function clearInputField() {
+  inputDOM.value = "";
+}
+
+function clearList() {
+  toDoList.innerHTML = "";
+}
+
+function addItem(item) {
+  if (item) {
+    toDo.push(item);
+  }
+}
+
+function generateList() {
+  if (!toDo) return;
+
+  toDo.forEach((item, i) => {
+    const liWrapper = document.createElement("div");
+    liWrapper.innerHTML = `
+        <li> ${item} </li>
+        <button>delete</button>
+    `;
+    liWrapper.addEventListener("click", () => {
+      deleteItem(i);
+    });
+    toDoList.appendChild(liWrapper);
+  });
+}
+
+function deleteItem(index) {
+  toDo.splice(index, 1);
+  clearList();
+  generateList();
+}
